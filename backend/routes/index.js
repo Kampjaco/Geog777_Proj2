4bbd0694f3fd4db44a825b980a5b9ecd43b2a4b1
Jacob Kampf <jkampf@wisc.edu>
2025-08-08 11:47:49 -0500

    still adding db connection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('./connection');
+const pool = require('../connection');
 
 // router.get('/', (req, res) => {
 //   res.send('Backend is working!');
