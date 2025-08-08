46c54c289431f76d65f600111abfc69d0c6562c5
Jacob Kampf <jkampf@wisc.edu>
2025-08-08 11:43:18 -0500

    Setting up queries to db

--- backend/api.js
+++ backend/api.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const pool = require('./db')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 
+//Middleware
 app.use(express.json());
 app.use('/', routes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('./connection');
 
-router.get('/', (req, res) => {
-  res.send('Backend is working!');
+// router.get('/', (req, res) => {
+//   res.send('Backend is working!');
+// });
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * from rides');
+    res.send(`Backend and DB connected: ${result.rows[0].now}`);
+  } catch (err) {
+    res.status(500).send('Database connection failed');
+  }
 });
 
 module.exports = router;
