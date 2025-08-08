89e16ae04442b76237ca01a1af31c159c1612ce9
Jacob Kampf <jkampf@wisc.edu>
2025-08-08 11:59:26 -0500

    triple ditto

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes');
-const pool = require('./db')
+const routes = require('./routes/index');
+const pool = require('./db/connection')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../db/connection');
 
 // router.get('/', (req, res) => {
 //   res.send('Backend is working!');
