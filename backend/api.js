536673d1de65cd187f93e3a2a752b036e26c3bd8
Jacob Kampf <jkampf@wisc.edu>
2025-08-10 18:36:14 -0500

    Have to set up cors for requests between front end and backend

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const pool = require('./db/connection')
+const pool = require('./db/connection');
+const cors = require('cors');
 require('dotenv').config();
 
 
@@ -11,6 +12,11 @@ const PORT = process.env.PORT || 3000;
 
 ///Middleware
 app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.use(cors({
+  origin: 'https://geog777-proj2-frontend.onrender.com'  // restrict to your frontend domain
+}));
+
 app.use(express.json());
 app.use('/api/rides', ridesRoutes);
 
